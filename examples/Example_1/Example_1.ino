dba78a74da388c8a9305ea0c1a1d7234e1131c9e
zapzrat <zapzrat@gmail.com>
2020-12-07 11:42:24 +0700

    Add files via upload

---
+++ examples/Example_1/Example_1.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+#include "TSL2561.h"
+
+TSL2561 tsl(TSL2561_ADDR_FLOAT);
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println("Micro Project Thailand ~^.^~ ");
+  if (tsl.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No sensor?");
+    while (1);
+  }
+
+
+  tsl.setGain(TSL2561_GAIN_16X);      // set 16x gain (for dim situations)
+  tsl.setTiming(TSL2561_INTEGRATIONTIME_13MS);  // shortest integration time (bright light)
+
+}
+
+void loop(void) {
+  uint16_t x = tsl.getLuminosity(TSL2561_VISIBLE);
+  Serial.println(x, DEC);
+
+  uint32_t lum = tsl.getFullLuminosity();
+  uint16_t ir, full;
+  ir = lum >> 16;
+  full = lum & 0xFFFF;
+  Serial.print("IR: "); Serial.print(ir);   Serial.print("\t\t");
+  Serial.print("Full: "); Serial.print(full);   Serial.print("\t");
+  Serial.print("Visible: "); Serial.print(full - ir);   Serial.print("\t");
+
+  Serial.print("Lux: "); Serial.println(tsl.calculateLux(full, ir));
+
+  delay(500);
+}
